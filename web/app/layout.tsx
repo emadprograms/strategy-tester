fc93f2e69b96fa3925cf1068ebbf33d1a060f97a
Hamza Arshad Alam <79793120+emadprograms@users.noreply.github.com>
2026-05-01 21:25:31 +0300

    feat: add favicon to metadata

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Strategy Tester Terminal",
   description: "Advanced Strategy Analysis Terminal",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
